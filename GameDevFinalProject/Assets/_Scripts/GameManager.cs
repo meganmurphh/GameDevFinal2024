23c69c2557aebc32104db0076c01a9f51c17e172
Megan Murphy <mlm4hb@umsystem.edu>
2024-11-26 21:27:28 -0600

    Scene 2 getting set up

--- GameDevFinalProject/Assets/_Scripts/GameManager.cs
+++ GameDevFinalProject/Assets/_Scripts/GameManager.cs
@@ -1,37 +1,42 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // For scene management
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] balloons; // Array to hold references to all the balloon objects
-    //public string nextSceneName = "NextLevel"; // The scene to load after all balloons are popped
+    public GameObject balloonsParent;
+    public string nextSceneName;
 
-    private int balloonsPopped = 0; // Keeps track of how many balloons have been popped
+    private int totalBalloons = 0; // Total number of balloons
+    private int balloonsPopped = 0; // Number of balloons popped
+
+    void Start()
+    {
+        // Get the total number of balloons (children of the Balloons parent)
+        totalBalloons = balloonsParent.transform.childCount;
+    }
 
     // Call this method from the PopBalloon script when a balloon is popped
     public void BalloonPopped()
     {
         // Increment the number of balloons popped
         balloonsPopped++;
-        Debug.Log("balloons popped:" + balloonsPopped);
 
         // Check if all balloons have been popped
-        if (balloonsPopped == balloons.Length)
+        if (balloonsPopped == totalBalloons)
         {
             // All balloons have been popped
             Debug.Log("All balloons popped! Level Complete!");
 
             // Optionally, load the next scene
-          //  LoadNextLevel();
+            LoadNextLevel();
         }
     }
 
     // Method to load the next level
-    //void LoadNextLevel()
-  //  {
-   //     SceneManager.LoadScene(nextSceneName);
-   // }
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
 }
